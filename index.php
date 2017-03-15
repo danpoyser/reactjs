b20c6c42a05bc65c8774571554a7d4825852cdd1
Daniel Poyser <poyser.1992@gmail.com>
2017-03-15 17:08:21 +0000

    react src files and HTML elements ready

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
-echo "this is doggo";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="js/react.min.js"></script>
+    <script src="js/react-dom.min.js"></script>
+    <script src="js/browser.min.js"></script>
+    <title>React Tutorials</title>
+  </head>
+
+  <body>
+    <div id="container"></div>
+
+    <script type="text/babel">
+
+
+    </script>
+  </body>
+</html>
